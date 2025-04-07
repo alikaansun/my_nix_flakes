ef482994362387c7b37fe2f86b62ea376909f416
alik <asunnetcoglu@gmail.com>
2025-04-07 22:56:38 +0200

    simple but wrong version

--- open-webui/flake.nix
+++ open-webui/flake.nix
@@ -1,30 +1,45 @@
 {
-description = "NixOS configuration with Ollama and Open WebUI";
+  description = "NixOS configuration with Ollama and Open WebUI";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    {
-      nixosConfigurations.ollama-system = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem 
+    (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      nixosConfigurations.ollama-system = nixpkgs.lib.nixosSystem 
+      {  
         modules = [
-          ({ pkgs, lib, ... }: 
+          ({ pkgs, lib, config, ... }: 
           {
+            # Enable unfree packages (required for CUDA)
+            nixpkgs.config.allowUnfree = true;
 
             # Enable Ollama service with CUDA acceleration
             services.ollama = {
               enable = true;
               acceleration = "cuda";
             };
-
             # Enable Open WebUI service
             services.open-webui.enable = true;
 
+            # Firewall settings to allow Open WebUI and Ollama communication
+            networking.firewall = {
+              enable = true;
+              allowedTCPPorts = [ 
+                3000  # Default Open WebUI port
+                8080  # Alternate port
+                11434 # Default Ollama API port
+              ];
+            };
           })
-          ];  
-          };
-    };
+        ];
+      };
+    
+    );
 }
