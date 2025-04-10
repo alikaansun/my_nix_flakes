80e13a9887e83e22a65658bad59227472a18b3b2
alik <asunnetcoglu@gmail.com>
2025-04-10 16:05:26 +0200

    added python basic env
    added python basic env313
    
    added python basic env313 with code

---
+++ pythonbasic/flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Python for basic tasks";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        
+        pythonEnv = pkgs.python313.withPackages (ps: with ps; [
+          decorator
+          ipython
+          kiwisolver
+          matplotlib
+          matplotlib-inline
+          numpy
+          pandas
+          pip # Ensure pip is explicitly included
+          scipy
+          setuptools # Needed for development installation
+          setuptools-scm
+          six
+          svgwrite
+          wheel # Useful for package building
+          pyclipper
+          pyyaml
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            pkgs.git
+          ];
+          
+          shellHook = ''
+            echo "Python basic development environment activated"
+            echo "Python version: $(python --version)"
+          
+            
+            # Launch VSCode
+            echo "Opening VSCode"
+            code &
+
+            echo "Development environment ready"
+          '';
+        };
+      }
+    );
+}
